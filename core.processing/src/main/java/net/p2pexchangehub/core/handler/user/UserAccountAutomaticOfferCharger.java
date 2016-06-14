b69478d5c55f5e5b6dbb58ff374c926616c4a8e8
Jiri Holy <nz@antiluminiscent.net>
2016-06-14 17:09:06 +1200

    replay fix

--- core.processing/src/main/java/net/p2pexchangehub/core/handler/user/UserAccountAutomaticOfferCharger.java
+++ core.processing/src/main/java/net/p2pexchangehub/core/handler/user/UserAccountAutomaticOfferCharger.java
@@ -11,15 +11,15 @@ import org.axonframework.eventhandling.annotation.EventHandler;
 import net.p2pexchangehub.core.api.offer.OfferMatchedEvent;
 import net.p2pexchangehub.core.api.user.CreditOfferFromUserAccountCommand;
 import net.p2pexchangehub.core.api.user.UserIncomingTransactionMatchedEvent;
+import net.p2pexchangehub.core.handler.AbstractIgnoreReplayEventHandler;
 import net.p2pexchangehub.core.handler.offer.OfferState;
 import net.p2pexchangehub.view.domain.Offer;
 import net.p2pexchangehub.view.repository.OfferRepository;
-import net.p2pexchangehub.view.repository.UserAccountRepository;
 
-public class UserAccountAutomaticOfferCharger {
-
-    @Inject
-    private UserAccountRepository userAccountRepository;
+/*
+ * Try to send money to offer from users wallet if possible.
+ */
+public class UserAccountAutomaticOfferCharger extends AbstractIgnoreReplayEventHandler {
 
     @Inject
     private OfferRepository offerRepository;
@@ -29,19 +29,23 @@ public class UserAccountAutomaticOfferCharger {
     
     @EventHandler
     public void incomingPayment(UserIncomingTransactionMatchedEvent event) {
-        Optional<Offer> oOffer = offerRepository.findByUserAccountIdAndState(event.getUserAccountId(), OfferState.WAITING_FOR_PAYMENT).stream()                
-            .filter(o -> o.getCurrencyOffered().equals(event.getAmount().getCurrencyCode()))
-            .filter(o -> o.getAmountOffered().compareTo(event.getNewBalance().getAmount())<=0)
-            .sorted(Comparator.comparing(Offer::getDateCreated))
-            .findFirst();
-        if (oOffer.isPresent()) {
-            commandGateway.send(new CreditOfferFromUserAccountCommand(oOffer.get().getId()));
+        if (isLive()) {
+            Optional<Offer> oOffer = offerRepository.findByUserAccountIdAndState(event.getUserAccountId(), OfferState.WAITING_FOR_PAYMENT).stream()                
+                    .filter(o -> o.getCurrencyOffered().equals(event.getAmount().getCurrencyCode()))
+                    .filter(o -> o.getAmountOffered().compareTo(event.getNewBalance().getAmount())<=0)
+                    .sorted(Comparator.comparing(Offer::getDateCreated))
+                    .findFirst();
+            if (oOffer.isPresent()) {
+                commandGateway.send(new CreditOfferFromUserAccountCommand(oOffer.get().getId()));
+            }
         }
     }
     
     @EventHandler
     public void offerMatched(OfferMatchedEvent event) {
-        commandGateway.send(new CreditOfferFromUserAccountCommand(event.getOfferId()));
+        if (isLive()) {
+            commandGateway.send(new CreditOfferFromUserAccountCommand(event.getOfferId()));            
+        }
     }
     
 }
